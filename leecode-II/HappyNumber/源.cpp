abf7a6ac1e353a6eaba6dc321d74b408f626047e
lisnb.pc <lisnb.h@hotmail.com>
2015-07-03 20:56:26 +0800

    20150703 - 2

---
+++ leecode-II/HappyNumber/源.cpp
@@ -0,0 +1,53 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> cache;
+        while (true)
+        {
+            int tmp = happy(n);
+            if (tmp == 1)
+                return true;
+            if (cache.find(tmp) != cache.end())
+                return false;
+            cache.insert(tmp);
+            n = tmp;
+        }
+        return false;
+    }
+
+private:
+    int happy(int n)
+    {
+        if (n == 1)
+            return 1;
+        //int frac = 10;
+        int sum(0), tmp(0);
+        while (n > 0)
+        {
+            tmp = n % 10;
+            sum += tmp*tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    vector<int> test = { 19, 28, 82, 68, 100 };
+    for (auto t : test)
+        cout << t << " " << s.isHappy(t) << endl;
+    system("pause");
+    return 0;
+}
