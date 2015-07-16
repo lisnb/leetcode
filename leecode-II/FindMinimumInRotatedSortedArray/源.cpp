117ddecf47c5c5addfc19f78cdc49f9b0e4b5f74
lisnb.pc <lisnb.h@hotmail.com>
2015-07-16 17:23:34 +0800

    20150716 - 1

---
+++ leecode-II/FindMinimumInRotatedSortedArray/源.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if (nums.empty())
+            return -1;
+        return findmin(nums, 0, nums.size() - 1);
+    }
+
+private:
+    int findmin(const vector<int> &nums, int b, int e)
+    {
+        if (b > e)
+            return -1;
+        if (b == e)
+            return nums.at(b);
+        if (nums.at(b) < nums.at(e))
+            return nums.at(b);
+        if (b + 1 == e)
+            return nums.at(e);
+        int mid = b + (e - b) / 2;
+        if (nums.at(mid) < nums.at(mid - 1))
+            return nums.at(mid);
+        else if (nums.at(mid) > nums.at(e))
+            return findmin(nums, mid + 1, e);
+        else
+            return findmin(nums, b, mid - 1);
+
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    vector<int> test = { 1, 1, 1,0,1};
+    cout << s.findMin(test) << endl;
+    system("pause");
+    return 0;
+}
